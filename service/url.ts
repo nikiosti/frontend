7482322f18432fa289a0282087925f5decdfbefb
nikiosti <yaostin@proton.me>
2024-02-29 21:59:25 +0300

    change: url release

--- service/url.ts
+++ service/url.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:8000/api/'
-export const BASE_TOKEN_REFRESH = 'http://localhost:8000/api/token/refresh/'
-export const BASE_TOKEN = 'http://localhost:8000/api/token/'
+export const BASE_URL = 'https://nikiostin.pythonanywhere.com/api/'
+export const BASE_TOKEN_REFRESH = 'https://nikiostin.pythonanywhere.com/api/token/refresh/'
+export const BASE_TOKEN = 'https://nikiostin.pythonanywhere.com/api/token/'
