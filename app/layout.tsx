64e175c55cc72025a8fd49d4a5afea47a41f070f
nikiosti <yaostin@proton.me>
2024-03-19 16:06:28 +0300

    add: YMetrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import '@mantine/dropzone/styles.css'
 
 import { ReactQueryMantineProvider } from '@/provider/ReactQueryMantineProvider'
 
+import { Metrika } from '@/components/Metrika/Metrika'
+import { Suspense } from 'react'
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -23,6 +26,9 @@ export default function RootLayout({
     <html lang="ru">
       <body className={inter.className}>
         <ReactQueryMantineProvider>{children}</ReactQueryMantineProvider>
+        <Suspense>
+          <Metrika />
+        </Suspense>
       </body>
     </html>
   )
